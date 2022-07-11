2fd964d7ee08d246b7cf0b2f87f60593b8965ea7
Arnaud-Bruchet <daikespana@gmail.com>
2022-07-11 12:50:52 +0200

    restructered project

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <div id="app">
-
+    <TeamCard v-for="(teamMate, index) in teams" :key="index" />
   </div>
 </template>
 
 <script>
 
+import teams from './assets/js/teams.js'
+import TeamCard from './components/TeamCard'
+
 export default {
   name: 'App',
+  data: () => ({
+    teams
+  }),
   components: {
-
+    TeamCard,
   }
 }
 </script>
---
+++ src/components/TeamCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="card">
+      <div class="card-image"></div>
+      <div class="card-text">
+        <span class="job">Full Stack 🎉</span>
+        <h2>FirstName & LastName</h2>
+        <p>verbatim</p>
+      </div>
+      <div class="card-stats">
+        <div class="stat">
+          <div class="techno">Techno</div>
+          <div class="level">⭐⭐⭐⭐</div>
+        </div>
+        <div class="stat border">
+          <div class="techno">Techno</div>
+          <div class="level">⭐⭐⭐</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TeamCard",
+  created() {},
+  data:() => ({
+
+  }),
+  props: {},
+  methods: {},
+};
+</script>
+
+<style scoped>
+@import "../assets/css/card_styles.css"
+</style>
